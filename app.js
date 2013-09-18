e3940a32a2fda92cf35659bdae25ce7ad3402353
Chris Hendel <chrisjh007@gmail.com>
2013-09-18 01:39:53 -0400

    Added basic Socket.io configuration.

--- app.js
+++ app.js
@@ -1,37 +1,64 @@
+// Production and development environment variables
+var isProduction = process.env.NODE_ENV === 'production';
+var devPort = 8888;
 
-/**
- * Module dependencies.
- */
-
+// Module dependencies
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+/**
+ * Setup Express and Socket.io
+ */
 var app = express();
+var server = app.listen(devPort);
+var io = require('socket.io').listen(server);
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+// All environments
+app.configure(function() {
+    app.set('port', process.env.PORT || devPort);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// Development only
+app.configure('development', function() {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
 
+// Socket connection config
+io.configure('production', function() {
+    io.enable('browser client etag');
+    io.set('log level', 1);
+
+    io.set('transports', [
+        'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
+    ]);
+});
+
+io.configure('development', function() {
+    io.set('transports', ['websocket']);
+});
+
+// http routes
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+/*http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});*/
+
+console.log('Express server listening on port ' + app.get('port'));
