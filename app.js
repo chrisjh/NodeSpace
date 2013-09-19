e8de2ec96722750b676c64861ec9329e09fcf6fa
Chris Hendel <chrisjh007@gmail.com>
2013-09-18 20:00:44 -0400

    Added Redis support. Created new Socket.io test connection.

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
+var redis = require('redis');
 
 /**
- * Setup Express and Socket.io
+ * Setup Express and Redis
  */
 var app = express();
+
+var client = redis.createClient();
+log('info', 'connected to redis server');
 
 // All environments
 app.configure(function() {
@@ -62,17 +66,60 @@ io.configure('development', function() {
     io.set('transports', ['websocket']);
 });
 
-io.sockets.on('connection', function(socket) {
+/*
+    Version 0.0.2 Test
+ */
+io.socket.on('connection', function(client) {
+    var subscribe = redis.createClient();
+    subscribe.subscribe('realtime');
+
+    subscribe.on("message", function(channel, message) {
+        client.send(message);
+        log('msg', "received from channel #" + channel + " : " + message);
+    });
+
+    client.on('message', function(msg) {
+        log('debug', msg);
+    });
+
+    client.on('disconnect', function() {
+        log('warn', 'disconnecting from redis');
+        subscribe.quit();
+    });
+});
+
+function log(type, msg) {
+
+    var color = '\u001b[0m',
+        reset = '\u001b[0m';
+
+    switch (type) {
+        case "info":
+            color = '\u001b[36m';
+            break;
+        case "warn":
+            color = '\u001b[33m';
+            break;
+        case "error":
+            color = '\u001b[31m';
+            break;
+        case "msg":
+            color = '\u001b[34m';
+            break;
+        default:
+            color = '\u001b[0m';
+    }
+    console.log(color + '   ' + type + '  - ' + reset + msg);
+}
+
+/*
+    Version 0.0.1 Test
+ */
+/*io.sockets.on('connection', function(socket) {
     socket.emit('news', {
         hello: 'world'
     });
     socket.on('my other event', function(data) {
         console.log(data);
     });
-});
-
-
-
-/*http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
 });*/
