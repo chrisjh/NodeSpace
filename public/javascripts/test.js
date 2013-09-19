468707a3f7c94fcd5f338da918dc92d3cd8806b2
Chris Hendel <chrisjh007@gmail.com>
2013-09-18 20:02:21 -0400

    Created new client test for socket.io and redis.

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,5 +1,37 @@
-var socket = io.connect('http://localhost');
+/*
+	Version 0.0.2 Test
+ */
+var socket = io.connect('http://localhost:8888/');
+
+socket.on('connect', function(data) {
+    setStatus('connected');
+    socket.emit('subscribe', {
+        channel: 'realtime'
+    });
+});
+
+socket.on('reconnecting', function(data) {
+    setStatus('reconnecting');
+});
+
+socket.on('message', function(data) {
+    console.log('received a message: ', data);
+    addMessage(data);
+});
+
+function addMessage(data) {
+    $('#online').html(data);
+}
+
+function setStatus(msg) {
+    console.log('Connection Status : ' + msg);
+}
+
+/*
+	Version 0.0.1 Test
+ */
+/*var socket = io.connect('http://localhost');
   socket.on('news', function (data) {
     console.log(data);
     socket.emit('my other event', { my: 'data' });
-  });
+  });*/
