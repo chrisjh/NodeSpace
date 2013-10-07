e675025dd5ecd6c1bc3b28a8f683f2c757cec095
James Martin <jamesml@live.unc.edu>
2013-10-07 12:06:42 -0400

    Updated Python bindings.

--- bindings/nodeBinding.py
+++ bindings/nodeBinding.py
@@ -1,6 +1,28 @@
 #https://github.com/invisibleroads/socketIO-client
 from socketIO_client import SocketIO
+import json
 
-with SocketIO('localhost', 8888) as socketIO:
-    socketIO.emit('aaa')
-    socketIO.wait(seconds=1)
+serverAddress = 'localhost'
+port = 8888
+
+def emit():
+	with SocketIO(serverAddress,port) as socketIO:
+		socketIO.emit('aaa')
+		socketIO.wait(seconds=1)
+
+def encode(o):
+	return JSONEncoder().encode(o)
+
+def put(tuple):
+	with SocketIO(serverAddress,port) as socketIO:
+		socketIO.emit('addDocument', tuple)
+
+def read(key):
+	print key
+
+#def get(key):
+
+print "Connecting"
+tuple = {'a':1}
+put(tuple)
+
