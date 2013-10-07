f4bfa82b185fdba6206b4ca12b4fde52c309df8c
James Martin <jamesml@live.unc.edu>
2013-10-07 14:08:22 -0400

    Added functionality to Python binding.

--- bindings/nodeBinding.py
+++ bindings/nodeBinding.py
@@ -2,24 +2,33 @@
 from socketIO_client import SocketIO
 import json
 
+#Needs to be set in client code
 serverAddress = 'localhost'
 port = 8888
 
+#Test function that simply connects to the server.
+#You can see if the connection works, the heartbeat of the server will reflect an increase in connection
 def emit():
 	with SocketIO(serverAddress,port) as socketIO:
 		socketIO.emit('aaa')
 		socketIO.wait(seconds=1)
 
+#Method to encode a Python object to JSON to be used as the argument of put(tuple)
 def encode(o):
 	return JSONEncoder().encode(o)
 
+#Put function
 def put(tuple):
 	with SocketIO(serverAddress,port) as socketIO:
 		socketIO.emit('addDocument', tuple)
 
+#Read function
 def read(key):
-	print key
+	with SocketIO(serverAddress,port) as socketIO:
+		socketIO.emit('findDocument', key)
+		#Need to wait for server response?
 
+#Get/take function
 #def get(key):
 
 print "Connecting"
