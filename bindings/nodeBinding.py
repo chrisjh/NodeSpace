5b5655f71e298bcc35509649fa572fbcfb02d9a8
James Martin <jamesml@live.unc.edu>
2013-11-10 19:10:12 -0500

    Changes to Python binding to include test files, removed Eclipse metadata from bindings directory.

--- bindings/nodeBinding.py
+++ bindings/nodeBinding.py
@@ -15,6 +15,7 @@
 
 from socketIO_client import SocketIO
 import json
+import time
 
 #Needs to be declared and set in client code
 SOCKET_IO_HOST = 'localhost'
@@ -22,12 +23,11 @@ SOCKET_IO_PORT = 8888
 socketIO = SocketIO(SOCKET_IO_HOST,SOCKET_IO_PORT)
 mostRecentTuple = ''
 
-def on_server_reply(*args):
-	print 'on_server_reply', args
 
-def emit():
-	socketIO.emit('aaa')
-	socketIO.wait(seconds=1)
+#Need to parse JSON object to be readable by any language
+def processTuple(*args):
+	print "Tuple read from space: "
+	print args
 
 #Method to encode a Python object to JSON to be used as the argument of put(tuple)
 def encode(o):
@@ -35,37 +35,60 @@ def encode(o):
 
 #Put function
 def put(tuple):
-	print tuple
+	print "Adding: "+tuple
 	socketIO.emit('addDocument', tuple)
 
 #Read function
 def read(key):
+	#try:
+	socketIO.on('foundDocument',processTuple)
 	socketIO.emit('findDocument', key)
-	
-	socketIO.on('foundDocument',on_server_reply)
-	#TODO:
-	#NEED TO FIX THIS WAITING TIME -- can probably do it without waiting...
-	#Need to keep function running until server reply is processed
-	socketIO.wait(seconds=1)
+	socketIO.wait_for_callbacks(seconds=3)
+	#except:
+		#print "Error or timeout"
 		
 #Get/take function
-#def take(key):
+def take(key):
+	socketIO.on('foundDocument',on_server_reply)
+	socketIO.emit('takeDocument', key)
+	socketIO.wait_for_callbacks(second=3)
+
+#
+# --- TESTING PUT ---
+#
+
+def putTest():
+	tupleOne = "hello,from,python"
+	tupleTwo = "foo"
+	#tupleThree = "bar,baz,boom"
+	emptyTuple = ""
+	try:
+		put(tupleOne)
+		put(tupleTwo)
+		put("bar,baz,boom")
+		put(emptyTuple)
+		put("")
+	except:
+		print "An add failed or a timeout occurred"
+
+#
+# --- TESTING READ ---
+#
+
+def readTest():
+	try:
+		read("hello,from,js")
+		read("hello,from,python")
+		read("hello")
+		read("")
+	except:
+		print "A read failed or a timeout occurred"
 
 # 
-# ---   TEST   --- 
+# ---   MAIN THREAD   --- 
 # 
 
 print "Connecting..."
-tuple = {'a': 'hello'}
-tup = 'hello'
-try:
-	put(tuple)
-	print "Add successful."
-except:
-	print "Add failed."
-try:
-	read(tup)
-	print "Read successful."
-	#print mostRecentTuple
-except:
-	print "Read failed."
+#putTest()
+readTest()
+print "Test complete."
