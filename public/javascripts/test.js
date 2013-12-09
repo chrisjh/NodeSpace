5cb9d1a934b48ec96d84dc8b29a9c2e734327c44
Chris Hendel <chrisjh007@gmail.com>
2013-12-09 08:05:27 -0500

    Added fill capabilities. Commented functions.

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,5 +1,5 @@
-/*
-    Version 0.1.1
+/**
+ * Establishes socket connection with server
  */
 var socket = io.connect('http://localhost:8888/', {
     'reconnect': true,
@@ -7,23 +7,39 @@ var socket = io.connect('http://localhost:8888/', {
     'max reconnection attempts': 5
 });
 
+/**
+ * Main socket function - enables all possible messages once connected to the server
+ * @param  var data [callback]
+ */
 socket.on('connect', function(data) {
     setStatus('connected');
 
+    /**
+     * Updates number of concurrently connected clients
+     * @param  var result [callback]
+     */
     socket.on('clientConnected', function(result) {
         $('#connections').html(result.count);
     });
 
+    /**
+     * Sends Put(T) function's input data to the space
+     * @return [emits message]
+     */
     $('#put').click(function() {
         var input = $('#putInput').val();
         socket.emit('addDocument', input);
     });
 
+    /**
+     * Response to Put(T) - Updates UI to reflect action
+     * @param  var result [callback]
+     */
     socket.on('addedDocument', function(result) {
 
         var resultStringify = JSON.stringify(result.data);
 
-        if (result.found == 'yes' && result.added == 'no') {
+        if (result.found === 'yes' && result.added === 'no') {
             $(".warn").show();
             $('#notifywarn').html("Put(T) Warning: Document already exists");
             $(".warn").fadeOut(5000);
@@ -32,7 +48,7 @@ socket.on('connect', function(data) {
             $('#status').html("Status: Document already exists in NodeSpace.")
             $('#returnedJSON').html(resultStringify);
             $('#returnedCSV').html(result.csv);
-        } else if (result.found == 'no' && result.added == 'yes') {
+        } else if (result.found === 'no' && result.added === 'yes') {
             $(".success").show();
             $('#notifysuccess').html("Put(T) Successful");
             $(".success").fadeOut(5000);
@@ -41,7 +57,7 @@ socket.on('connect', function(data) {
             $('#status').html("Status: Document inserted into NodeSpace")
             $('#returnedJSON').html(resultStringify);
             $('#returnedCSV').html(result.csv);
-        } else if (result.found == 'no' && result.added == 'no') {
+        } else if (result.found === 'no' && result.added === 'no') {
             $(".error").show();
             $('#notifyerror').html("Put(T) Error: Document insert failed");
             $(".error").fadeOut(5000);
@@ -49,23 +65,31 @@ socket.on('connect', function(data) {
             console.log(result);
             $('#status').html("Status: Document failed to be inserted into NodeSpace.")
             $('#added').html("Added? " + result.added);
-            if (result.error == 'yes') {
+            if (result.error === 'yes') {
                 $('#errorType').html("Error on " + result.errorType);
                 $('#errorMsg').html(result.errorMsg);
             }
         }
     });
 
+    /**
+     * Sends Read(T) function's input data to the space
+     * @return [emits message]
+     */
     $('#read').click(function() {
         var input = $('#readInput').val();
         socket.emit('findDocument', input);
     });
 
+    /**
+     * Response to Read(T) - Updates UI to reflect action
+     * @param  var result [callback]
+     */
     socket.on('foundDocument', function(result) {
 
         var resultStringify = JSON.stringify(result.data);
 
-        if (result.found == 'yes') {
+        if (result.found === 'yes') {
             $(".success").show();
             $('#notifysuccess').html("Read(T) Successful");
             $(".success").fadeOut(5000);
@@ -74,7 +98,7 @@ socket.on('connect', function(data) {
             $('#status').html("Status: Document found in NodeSpace")
             $('#returnedJSON').html(resultStringify);
             $('#returnedCSV').html(result.csv);
-        } else if (result.found == 'no') {
+        } else if (result.found === 'no') {
             $(".error").show();
             $('#notifyerror').html("Read(T) Error: Document not found");
             $(".error").fadeOut(5000);
@@ -83,23 +107,31 @@ socket.on('connect', function(data) {
             $('#status').html("Status: Document not found in NodeSpace.")
             $('#returnedJSON').html(resultStringify);
             $('#returnedCSV').html(result.csv);
-            if (result.error == 'yes') {
+            if (result.error === 'yes') {
                 $('#errorType').html("Error on " + result.errorType);
                 $('#errorMsg').html(result.errorMsg);
             }
         }
     });
 
+    /**
+     * Sends Take(T) function's input data to the space
+     * @return [emits message]
+     */
     $('#take').click(function() {
         var input = $('#takeInput').val();
         socket.emit('takeDocument', input);
     });
 
+    /**
+     * Response to Take(T) - Updates UI to reflect action
+     * @param  var result [callback]
+     */
     socket.on('tookDocument', function(result) {
 
         var resultStringify = JSON.stringify(result.data);
 
-        if (result.found == 'yes') {
+        if (result.found === 'yes') {
             $(".success").show();
             $('#notifysuccess').html("Take(T) Successful");
             $(".success").fadeOut(5000);
@@ -108,7 +140,7 @@ socket.on('connect', function(data) {
             $('#status').html("Status: Document found and removed from NodeSpace")
             $('#returnedJSON').html(resultStringify);
             $('#returnedCSV').html(result.csv);
-        } else if (result.found == 'no') {
+        } else if (result.found === 'no') {
             $(".error").show();
             $('#notifyerror').html("Take(T) Error: Document not found");
             $(".error").fadeOut(5000);
@@ -117,29 +149,64 @@ socket.on('connect', function(data) {
             $('#status').html("Status: Document not found in NodeSpace.")
             $('#returnedJSON').html(resultStringify);
             $('#returnedCSV').html(result.csv);
-            if (result.error == 'yes') {
+            if (result.error === 'yes') {
                 $('#errorType').html("Error on " + result.errorType);
                 $('#errorMsg').html(result.errorMsg);
             }
         }
     });
 
+    /**
+     * Sends command to drop the space
+     * @return [emits message]
+     */
     $('#drop').click(function() {
         socket.emit('dropSpace');
     });
 
+    /**
+     * Response to drop() - Updates UI to reflect action
+     * @param  var data [callback]
+     */
     socket.on('spaceDropped', function(data) {
         console.log(data.isDropped);
-        if (data.isDropped == 'yes') {
+        if (data.isDropped === 'yes') {
             $(".success").show();
             $('#notifysuccess').html("NodeSpace Dropped Successfully");
             $(".success").fadeOut(5000);
-            console.log('Space dropped');
-            $('#foundTuple').html("Found tuple?");
-            $('#returnedTuple').html("None");
+            console.log('### NodeSpace dropped successfully');
+            $('#status').html("Status: NodeSpace has been dropped.")
+            $('#returnedJSON').html(resultStringify);
+            $('#returnedCSV').html(result.csv);
         }
     });
 
+    /**
+     * Sends command to populate the space
+     * @return [emits message]
+     */
+    $('#fill').click(function() {
+        socket.emit('fillSpace');
+    });
+
+    /**
+     * Response to populating the space - Updates UI to reflect action
+     * @param  var data [callback]
+     */
+    socket.on('spaceFilled', function(data) {
+        console.log(data.isFilled);
+        if (data.isFilled === 'yes') {
+            $(".success").show();
+            $('#notifysuccess').html("NodeSpace Populated Successfully");
+            $(".success").fadeOut(5000);
+            console.log('### NodeSpace Populated successfully');
+            $('#status').html("Status: NodeSpace has been populated.")
+        }
+    });
+
+    /**
+     * Parses the raw JSON to a more human readable format
+     */
     $('#prettify').click(function() {
         var text = $('#returnedJSON').text();
         var json = JSON.parse(text);
@@ -150,9 +217,18 @@ socket.on('connect', function(data) {
 
 });
 
+/**
+ * Sets status when connection to the server is lost
+ * @param  var data [callback]
+ */
 socket.on('reconnecting', function(data) {
     setStatus('reconnecting');
 });
+
+/**
+ * Modifies status in browser console
+ * @param String msg
+ */
 
 function setStatus(msg) {
     console.log('Connection Status : ' + msg);
