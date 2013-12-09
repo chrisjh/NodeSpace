20eb13cd72029931c6120a48bcb8ef7990fe7cae
Chris Hendel <chrisjh007@gmail.com>
2013-12-09 05:09:40 -0500

    Updated connection info and cleaned up functions.

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -3,64 +3,25 @@
  */
 var socket = io.connect('http://localhost:8888/', {
     'reconnect': true,
-    'reconnection delay': 500,
+    'reconnection delay': 1000,
     'max reconnection attempts': 5
-});
-
-var pages = {};
-var lastPageId = 0;
-
-function GetInfo() {
-    socket.emit('GetSession', '');
-    // The client is now initiating the request we setup earlier to get its socket.id from the server.
-}
-
-socket.on('SessionID', function(msg) {
-    socket.emit('SendMessage', msg + ' Has Connected');
-    // When the server returns the socket.id the client will emit a message to the server for anyone
-    //  else who is subscribed to the "message" channel. My terminology for these explanations is not
-    //   accurate but I think that this way of stating things is a lot easier to wrap your head around...
 });
 
 socket.on('connect', function(data) {
     setStatus('connected');
 
-    socket.on('pageview', function(msg) {
-        $('#connections').html(msg.connections);
-        if (msg.url) {
-            if ($('#visits tr').length > 10) {
-                $('#visits tr:last').remove();
-            }
-            $('#visits tbody').prepend('<tr><td>' + msg.url + '</td><td>' + msg.ip + '</td><td>' + msg.timestamp + '</td></tr>');
-
-            if (pages[msg.url]) {
-                pages[msg.url].views = pages[msg.url].views + 1;
-                $('#page' + pages[msg.url].pageId).html(pages[msg.url].views);
-            } else {
-                pages[msg.url] = {
-                    views: 1,
-                    pageId: ++lastPageId
-                };
-                $('#pageViews tbody').append('<tr><td>' + msg.url + '</td><td id="page' + lastPageId + '">1</td></tr>');
-            }
-        }
+    socket.on('clientConnected', function(result) {
+        $('#connections').html(result.count);
     });
 
     $('#put').click(function() {
-        console.log('Putting tuple in the space...');
-
         var input = $('#putInput').val();
-
-        console.log(input);
-
         socket.emit('addDocument', input);
     });
 
     socket.on('addedDocument', function(result) {
 
         var resultStringify = JSON.stringify(result.data);
-
-        console.log(result.found);
 
         if (result.found == 'yes' && result.added == 'no') {
             $(".warn").show();
@@ -97,15 +58,12 @@ socket.on('connect', function(data) {
 
     $('#read').click(function() {
         var input = $('#readInput').val();
-
         socket.emit('findDocument', input);
     });
 
     socket.on('foundDocument', function(result) {
 
         var resultStringify = JSON.stringify(result.data);
-
-        console.log(result.found);
 
         if (result.found == 'yes') {
             $(".success").show();
@@ -134,15 +92,12 @@ socket.on('connect', function(data) {
 
     $('#take').click(function() {
         var input = $('#takeInput').val();
-
         socket.emit('takeDocument', input);
     });
 
     socket.on('tookDocument', function(result) {
 
         var resultStringify = JSON.stringify(result.data);
-
-        console.log(result.found);
 
         if (result.found == 'yes') {
             $(".success").show();
@@ -198,15 +153,6 @@ socket.on('connect', function(data) {
 socket.on('reconnecting', function(data) {
     setStatus('reconnecting');
 });
-
-socket.on('message', function(data) {
-    console.log('received a message: ', data);
-    addMessage(data);
-});
-
-function addMessage(data) {
-    $('#online').html(data);
-}
 
 function setStatus(msg) {
     console.log('Connection Status : ' + msg);
